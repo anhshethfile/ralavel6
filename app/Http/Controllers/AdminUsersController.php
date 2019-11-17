0888977135c50d0d9d0efb175dff5631795861e9
anhshethfile <anhshethfile@gmail.com>
2019-11-17 12:22:27 +0700

    All uploads
    finish sent

---
+++ app/Http/Controllers/AdminUsersController.php
@@ -0,0 +1,95 @@
+<?php
+namespace App\Http\Controllers;
+use App\AdminUsers;
+use Illuminate\Http\Request;
+class AdminUsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+       // return "hi";
+
+       $data['users']=AdminUsers::all();
+
+      return view('dashboard.users.index',compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('dashboard.users.add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // return $request->all();
+
+        $ticket = new Ticket();
+        $data = $this->validate($request, [ 'description'=>'required', 'title'=> 'required' ]);
+
+        return redirect('/home')->with('success', 'New support ticket has been created! Wait sometime to get resolved');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return view('dashboard.users.show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        return view('dashboard.users.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
